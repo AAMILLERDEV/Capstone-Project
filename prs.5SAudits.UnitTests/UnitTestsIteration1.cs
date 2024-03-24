874dd2e396b30a6d54c904982747658a85437609
Aaron <aajmiller12@gmail.com>
2024-03-24 19:24:04 -0400

    Added xUnit Selenium tests to vs

--- prs.5SAudits.UnitTests/UnitTestsIteration1.cs
+++ prs.5SAudits.UnitTests/UnitTestsIteration1.cs
@@ -12,94 +12,95 @@ public class UnitTestsIteration1
         db = new DBSQLRepository("Server=.;Database=prs.5SAudits;User ID=sa; Password=password;Trusted_Connection=True;");
     }
 
-    [Fact]
-    public async void GetAudits()
-    {
-        var audits = await db.GetAudits();
-        Assert.NotNull(audits);
-    }
+    //[Fact]
+    //public async void GetAudits()
+    //{
+    //    var audits = await db.GetAudits();
+    //    Assert.NotNull(audits);
+    //}
 
-    [Fact]
-    public async void GetActions()
-    {
-        var actions = await db.GetActions();
-        Assert.NotNull(actions);
-    }
+    //[Fact]
+    //public async void GetActions()
+    //{
+    //    var actions = await db.GetActions();
+    //    Assert.NotNull(actions);
+    //}
 
-    [Fact]
-    public async void GetAuditLog()
-    {
-        var auditLogs = await db.GetAuditLog();
-        Assert.NotNull(auditLogs);
-    }
+    //[Fact]
+    //public async void GetAuditLog()
+    //{
+    //    var auditLogs = await db.GetAuditLog();
+    //    Assert.NotNull(auditLogs);
+    //}
 
-    [Fact]
-    public async void GetAuditStatus()
-    {
-        var auditStatus = await db.GetAuditStatus();
-        Assert.NotNull(auditStatus);
-    }
+    //[Fact]
+    //public async void GetAuditStatus()
+    //{
+    //    var auditStatus = await db.GetAuditStatus();
+    //    Assert.NotNull(auditStatus);
+    //}
 
-    [Fact]
-    public async void GetCheckItem()
-    {
-        var checkItem = await db.GetCheckItem();
-        Assert.NotNull(checkItem);
-    }
+    //[Fact]
+    //public async void GetCheckItem()
+    //{
+    //    var checkItem = await db.GetCheckItem();
+    //    Assert.NotNull(checkItem);
+    //}
 
-    [Fact]
-    public async void GetDeductions()
-    {
-        var deductions = await db.GetDeductions();
-        Assert.NotNull(deductions);
-    }
+    //[Fact]
+    //public async void GetDeductions()
+    //{
+    //    var deductions = await db.GetDeductions();
+    //    Assert.NotNull(deductions);
+    //}
 
-    [Fact]
-    public async void GetResources()
-    {
-        var resources = await db.GetResources();
-        Assert.NotNull(resources);
-    }
+    //[Fact]
+    //public async void GetResourcesByID()
+    //{
+    //    var id = 1;
+    //    var resources = await db.GetResourcesByAuditId(1);
+    //    Assert.NotNull(resources);
+    //}
 
-    [Fact]
-    public async void GetScores()
-    {
-        var scores = await db.GetScores();
-        Assert.NotNull(scores);
-    }
+    //[Fact]
+    //public async void GetScores()
+    //{
+    //    var scores = await db.GetScores();
+    //    Assert.NotNull(scores);
+    //}
 
-    [Fact]
-    public async void GetScoringCategories()
-    {
-        var scoringCategories = await db.GetScoringCategories();
-        Assert.NotNull(scoringCategories);
-    }
+    //[Fact]
+    //public async void GetScoringCategories()
+    //{
+    //    var scoringCategories = await db.GetScoringCategories();
+    //    Assert.NotNull(scoringCategories);
+    //}
 
-    [Fact]
-    public async void GetScoringCriteria()
-    {
-        var scoringCriteria = await db.GetScoringCriteria();
-        Assert.NotNull(scoringCriteria);
-    }
+    //[Fact]
+    //public async void GetScoringCriteria()
+    //{
+    //    var scoringCriteria = await db.GetScoringCriteria();
+    //    Assert.NotNull(scoringCriteria);
+    //}
     
-    [Fact]
-    public async void GetSettings()
-    {
-        var settings = await db.GetSettings();
-        Assert.NotNull(settings);
-    }
+    //[Fact]
+    //public async void GetSettings()
+    //{
+    //    var settings = await db.GetSettings();
+    //    Assert.NotNull(settings);
+    //}
 
-    [Fact]
-    public async void GetZoneCategories()
-    {
-        var zoneCategories = await db.GetZoneCategories();
-        Assert.NotNull(zoneCategories);
-    }
+    //[Fact]
+    //public async void GetZoneCategories()
+    //{
+    //    var zoneCategories = await db.GetZoneCategories();
+    //    Assert.NotNull(zoneCategories);
+    //}
 
-    [Fact]
-    public async void GetZones()
-    {
-        var zones = await db.GetZones();
-        Assert.NotNull(zones);
-    }
+    //[Fact]
+    //public async void GetZones()
+    //{
+    //    var zones = await db.GetZones();
+    //    Assert.NotNull(zones);
+    //}
 }
---
+++ prs.5SAudits.UnitTests/WebTestsIteration3.cs
@@ -0,0 +1,108 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using Xunit;
+
+namespace prs._5SAudits.UnitTests
+{
+    public class WebTestsIteration3 : IDisposable
+    {
+        public IWebDriver driver { get; private set; }
+        public IDictionary<String, Object> Vars { get; private set; }
+        public IJavaScriptExecutor Js { get; private set; }
+
+        const int timerLength = 500;
+
+        public WebTestsIteration3()
+        {
+            driver = new ChromeDriver();
+            Js = (IJavaScriptExecutor)driver;
+            Vars = new Dictionary<String, Object>();
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+
+        [Fact]
+        public void StartingAnAudit_TestOne()
+        {
+
+            driver.Navigate().GoToUrl("http://localhost:4200/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1450, 1050);
+
+            Thread.Sleep(timerLength);
+
+            driver.FindElement(By.Id("newAuditButton")).Click();
+
+            Thread.Sleep(timerLength);
+
+            driver.FindElement(By.Id("zoneCategory")).Click();
+            {
+                var dropdown = driver.FindElement(By.Id("zoneCategory"));
+                dropdown.FindElement(By.XPath("//option[. = 'Stamping']")).Click();
+            }
+
+            Thread.Sleep(timerLength);
+            driver.FindElement(By.CssSelector(".form-select-sm")).Click();
+            {
+                var dropdown = driver.FindElement(By.CssSelector(".form-select-sm"));
+                dropdown.FindElement(By.XPath("//option[. = 'West (M485, M470-5, M455, M460)']")).Click();
+            }
+
+            Thread.Sleep(timerLength);
+
+            driver.FindElement(By.CssSelector(".btn")).Click();
+
+            Thread.Sleep(timerLength);
+
+            Assert.Equal(driver.FindElement(By.CssSelector(".fs-4")).Text, "Sort");
+        }
+
+        [Fact]
+        public void EditingAnAudit_TestTwo()
+        {
+            driver.Navigate().GoToUrl("http://localhost:4200/home");
+            driver.Manage().Window.Size = new System.Drawing.Size(1936, 1066);
+
+            Thread.Sleep(timerLength);
+
+            driver.FindElement(By.CssSelector(".input-group:nth-child(2) #floatingInputGroup1")).Click();
+            driver.FindElement(By.CssSelector(".input-group:nth-child(2) #floatingInputGroup1")).SendKeys("1");
+
+            Thread.Sleep(timerLength);
+
+
+            driver.FindElement(By.LinkText("Edit")).Click();
+            driver.FindElement(By.CssSelector(".btn")).Click();
+            {
+                var element = driver.FindElement(By.CssSelector(".btn"));
+                Actions builder = new Actions(driver);
+                builder.MoveToElement(element).Perform();
+            }
+
+            Thread.Sleep(timerLength);
+
+            driver.FindElement(By.Id("scoreSelection")).Click();
+            {
+                var dropdown = driver.FindElement(By.Id("scoreSelection"));
+                dropdown.FindElement(By.XPath("//option[. = '3']")).Click();
+            }
+            
+            Thread.Sleep(timerLength);
+
+            driver.FindElement(By.Id("btnNext")).Click();
+
+            Thread.Sleep(timerLength);
+
+
+            driver.FindElement(By.Id("btnPrevious")).Click();
+            {
+                String value = driver.FindElement(By.Id("scoreSelection")).GetAttribute("value");
+                Assert.Equal(value, "3");
+            }
+        }
+
+    }
+}
